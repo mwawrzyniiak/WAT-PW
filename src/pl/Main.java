1216e6698056bd5503ac8fbf870ad27a181ecdd2
Maciej Wawrzyniak <35369071+zxcv22z@users.noreply.github.com>
2019-05-14 18:04:06 +0200

    Create DistributorQueue Class (Thread) with simple function

--- src/pl/Main.java
+++ src/pl/Main.java
@@ -1,12 +1,30 @@
 package pl;
 
+import pl.entity.DistributorQueue;
 import pl.entity.Station;
 import pl.service.CarGenerator;
 
 public class Main {
     public static void main(String[] args) {
-        CarGenerator carGenerator = new CarGenerator();
+        //Test działania wątków:
+        /*CarGenerator carGenerator = new CarGenerator();
+        DistributorQueue distributorQueue1 = new DistributorQueue(1);
+        DistributorQueue distributorQueue2 = new DistributorQueue(2);
+        DistributorQueue distributorQueue3 = new DistributorQueue(3);
+        DistributorQueue distributorQueue4 = new DistributorQueue(4);
+        DistributorQueue distributorQueue5 = new DistributorQueue(5);
+        DistributorQueue distributorQueue6 = new DistributorQueue(6);
+        DistributorQueue distributorQueue7 = new DistributorQueue(7);
+        DistributorQueue distributorQueue8 = new DistributorQueue(8);
         carGenerator.start();
+        distributorQueue1.start();
+        distributorQueue2.start();
+        distributorQueue3.start();
+        distributorQueue4.start();
+        distributorQueue5.start();
+        distributorQueue6.start();
+        distributorQueue7.start();
+        distributorQueue8.start();*/
     }
 
 
--- src/pl/entity/DistributorQueue.java
+++ src/pl/entity/DistributorQueue.java
@@ -8,9 +8,37 @@ package pl.entity;
     Ustalamy zasadę, że samochód zawsze podjeżdża pod dystrybutor który stoi dalej, i nie blokuje dystrybutora
 */
 
+import java.util.LinkedList;
+import java.util.Random;
+
 //TODO: sekcja krytyczna
 //Sekcja krytyczna
-public class DistributorQueue {
+public class DistributorQueue extends Thread{
 
+    private static final boolean WORK = true;
 
+    private LinkedList<Car> distributorQueueForcar = new LinkedList<Car>();
+    private Integer QUEUE_ID;
+    private Random random = new Random();
+
+    public DistributorQueue(Integer QUEUE_ID) {
+        this.QUEUE_ID = QUEUE_ID;
+    }
+
+    public void run() {
+        while(WORK) {
+            canITakeCar();
+            try {
+                Thread.sleep(random.nextInt(1500) + 500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //TODO: SPRAWDZA CZY JEST DOSTEPNY SAMOCHOD
+    public boolean canITakeCar() {
+        System.out.println("SPRAWDZAM CZY MOGE WZIAC SAMOCHOD" + QUEUE_ID);
+        return true;
+    }
 }
